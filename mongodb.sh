a9bc1b704d19d35dbc39cd25f33b98664c0eece6
NanduCn <chiranjeevikuracha@gmail.com>
2023-01-28 01:15:39 +0530

    coonfiguration file of frontend

--- mongodb.sh
+++ mongodb.sh
@@ -3,5 +3,5 @@ cp ${script_location}/files/mogodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org -yum
 systemctl enable mongod
 systemctl start mongod
-sed -i -e 's/127.0.0.1/0.0.0.0' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
