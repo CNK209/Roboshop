18b4ed7843406372a502186640521bc70cec4b38
NanduCn <chiranjeevikuracha@gmail.com>
2023-01-29 16:01:30 +0530

    create servers script

--- frontend.sh
+++ frontend.sh
@@ -11,17 +11,41 @@ if [ $? -eq 0 ];then
 
 echo -e "\e[32m Remove nginx old content\e[0m"
 rm -rf /usr/share/nginx/html/* &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+    fi
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
 cd /usr/share/nginx/html &>>${LOG}
 
 echo -e "\e[34m Extract frontend content\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+    fi
 
 echo -e "\e[33m copy roboshop nginx config file\e[0m"
 cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 
+if [ $? -eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+    fi
 echo -e "\e[32m Enable Nginx\e[0m"
 systemctl enable nginx &>>${LOG}
-
+if [ $? -eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+    fi
 echo -e "\e[35m Restart nginx\e[0m"
 systemctl restart nginx &>>${LOG}
+if [ $? -eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+    fi
