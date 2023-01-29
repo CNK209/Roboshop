bd4b32c2b2c16831634f308bdf019158839acc76
NanduCn <chiranjeevikuracha@gmail.com>
2023-01-29 16:40:26 +0530

    create servers script

--- catalogue.sh
+++ catalogue.sh
@@ -1,21 +1,104 @@
 script_location=$(pwd)
-set -e
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+LOG=/tmp/roboshop.log
+echo -e "\e[33m configuring node js repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash $>>${LOG}
+if [ $? eq 0 ]then;
+echo SUCCESS
+else
+  echo FAILED
+  fi
+echo -e "\e[32m install nodejs\e[0m"
+yum install nodejs -y &>>${LOG}
+if [ $? eq 0];then
+  echo SUCCESS
+  else
+    echo FAILED
+    fi
 useradd roboshop
 mkdir -p /app
+echo -e "\e[33m downloading catalogue files\e[0m"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-rm -rf /app/*
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl start catalogue
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
-mongo --host 172.31.6.119 </app/schema/catalogue.js
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+    fi
+    echo -e "\e[34m removing old content\e[0m"
+rm -rf /app/* &>>${LOG}
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+    fi
 
+cd /app
+echo -e "\e[33m extracting catalogue file\e[0m"
+unzip /tmp/catalogue.zip
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+    fi
+
+cd /app
+echo -e "\e[34m installing npm\e[0m"
+
+npm install
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+    fi
+    echo -e "\e[34m confguring node js repos\e[0m"
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+      fi
+echo -e "\e[33m starting catalogue service\e[0m"
+systemctl daemon-reload
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+      fi
+echo -e "\e[33m enabling catalogue service\e[0m"
+systemctl enable catalogue
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+      fi
+echo -e "\e[33m starting catalogue service\e[0m"
+systemctl start catalogue
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+      fi
+echo -e "\e[33m confguring mongodb service\e[0m"
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo $>>${LOG}
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+      fi
+echo -e "\e[33m INSTALLING mongodb client\e[0m"
+
+yum install mongodb-org-shell -y $>>${LOG}
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+      fi
+
+echo -e "\e[33m loading mongodb schema\e[0m"
+mongo --host 172.31.6.119 </app/schema/catalogue.js $>>${LOG}
+
+if [ $? eq 0 ];then
+  echo SUCCESS
+  else
+    echo FAILED
+      fi
 
