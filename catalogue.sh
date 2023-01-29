42d3a551b47e52e2cd94f5ab272cfb491c46060e
NanduCn <chiranjeevikuracha@gmail.com>
2023-01-29 16:50:28 +0530

    create servers script

--- catalogue.sh
+++ catalogue.sh
@@ -1,7 +1,7 @@
 script_location=$(pwd)
 LOG=/tmp/roboshop.log
 echo -e "\e[33m configuring node js repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash $>>${LOG}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 if [ $? -eq 0 ];then
 echo SUCCESS
 else
