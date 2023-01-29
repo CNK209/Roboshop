d200fea0ba041a602cce9e41a23cd5b42faac00a
NanduCn <chiranjeevikuracha@gmail.com>
2023-01-29 13:11:00 +0530

    create servers script

--- catalogue.sh
+++ catalogue.sh
@@ -2,7 +2,7 @@ script_location=$(pwd)
 set -e
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-#useradd roboshop
+useradd roboshop
 mkdir -p /app
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 rm -rf /app/*
@@ -17,4 +17,5 @@ systemctl start catalogue
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
 mongo --host 172.31.6.119 </app/schema/catalogue.js
+
 
