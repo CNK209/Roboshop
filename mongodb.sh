f472511b4ae681b86c0aa00c2261c52a45d911e4
NanduCn <chiranjeevikuracha@gmail.com>
2023-01-29 21:33:48 +0530

    special variabls

--- mongodb.sh
+++ mongodb.sh
@@ -1,9 +1,19 @@
 script_location=$(pwd)
+print_head "copy mongodb repo files"
 cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-systemctl restart mongod
+STATUS_CHECK
+print_head "Install Mongodb"
+yum install mongodb-org -y &>>{$LOG}
+STATUS_CHECK
+print_head "Update Mongodb listen address"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>{$LOG}
+STATUS_CHECK
 
+print_head "Install Mongodb"
+systemctl enable mongod &>>{$LOG}
+STATUS_CHECK
+
+print_head "install mongodb"
+systemctl restart mongod &>>{$LOG}
+STATUS_CHECK
 
