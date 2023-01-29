9088603476a5f876d8d6f634eec41d5b018feadb
NanduCn <chiranjeevikuracha@gmail.com>
2023-01-29 17:47:01 +0530

    create servers script

--- catalogue.sh
+++ catalogue.sh
@@ -1,14 +1,4 @@
-script_location=$(pwd)
-LOG=/tmp/roboshop.log
-STATUS_CHECK(){
-  if [ $? -eq 0 ];then
-    echo -e "\e[32m SUCCESS \e[0m"
-    else
-      echo -e "\e[31m FAILED \e[0m"
-      echo "refer log files for more information LOG - ${LOG}"
-  exit
-      fi
-}
+source common.sh
 echo -e "\e[33m configuring node js repos\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
 STATUS_CHECK
