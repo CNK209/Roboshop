7113de14b4a65a88034ce69d167b36932bae14e4
NanduCn <chiranjeevikuracha@gmail.com>
2023-02-24 17:18:06 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -26,4 +26,6 @@ STATUS_CHECK
 
 print_head "Reset Default Database Password"
 mysql_secure_installation --set-root-pass ${root_mysql_password} &>>${LOG}
+if [ $? -eq 1 ]; then
+  echo "password is already changed"
 STATUS_CHECK
